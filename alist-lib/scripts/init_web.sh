d48ddb9fd4d16d5678d355ea881c4a274da7feb6
j2rong4cn <253551464@qq.com>
2025-01-08 17:29:17 +0800

    支持alist beta版本

--- alist-lib/scripts/init_web.sh
+++ alist-lib/scripts/init_web.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
 
-curl -L https://github.com/alist-org/alist-web/releases/latest/download/dist.tar.gz -o dist.tar.gz
+if [ -n "$1" ]; then
+  curl -L https://codeload.github.com/alist-org/web-dist/tar.gz/refs/heads/dev -o web-dist-dev.tar.gz
+  tar -zxvf web-dist-dev.tar.gz
+  rm -rf web-dist-dev.tar.gz
+  rm -rf ../public/dist
+  mv -f web-dist-dev/dist ../public
+  exit
+fi
+
+curl -LO https://github.com/alist-org/alist-web/releases/latest/download/dist.tar.gz
 tar -zxvf dist.tar.gz
+rm -rf dist.tar.gz
 rm -rf ../public/dist
 mv -f dist ../public
-rm -rf dist.tar.gz
